3eb9e3421849ea321bb45d1bcdaa9a5f18eda808
gr3y6h0st <jayvy19@gmail.com>
2020-06-07 21:33:10 -0700

    Connected Roll Button from MainActivity.kt

--- app/src/main/java/com/templates/diceroller/MainActivity.kt
+++ app/src/main/java/com/templates/diceroller/MainActivity.kt
@@ -2,11 +2,19 @@ package com.templates.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    val rollButton: Button = findViewById(R.id.roll_button)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rollButton.text = "Let's Roll"
+
+
+
     }
 }
