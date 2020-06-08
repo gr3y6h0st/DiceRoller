375bce406d8baf6d2e0f3c3f2db55945c361fc08
gr3y6h0st <jayvy19@gmail.com>
2020-06-07 21:47:00 -0700

    modified rollDice() to generate random numbers 1-6.

--- app/src/main/java/com/templates/diceroller/MainActivity.kt
+++ app/src/main/java/com/templates/diceroller/MainActivity.kt
@@ -3,7 +3,9 @@ package com.templates.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,8 +16,15 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.text = "Let's Roll"
         rollButton.setOnClickListener {
-            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text_view)
+        val randomInt = Random().nextInt(6)+1
+        resultText.text = randomInt.toString()
 
     }
 }
